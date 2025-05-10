22e730fbb82d3e6e255b8974c8e3739004ca75a0
kych4n <hades1@kakao.com>
2025-05-10 22:44:17 +0900

    feat [#3] 응답 형식 정의

---
+++ src/main/java/cotato/five/weather/common/SuccessResponse.java
@@ -0,0 +1,29 @@
+package cotato.five.weather.common;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum SuccessResponse {
+    REGISTERED_LOCATION(HttpStatus.CREATED, "LOC-001", "위치 등록 성공"),
+    RETRIEVED_LIST_LOCATION(HttpStatus.OK, "LOC-002", "위치 목록 조회 성공"),
+    RETRIEVED_DETAIL_LOCATION(HttpStatus.OK, "LOC-003", "위치 상세 조회 성공"),
+    DELETED_LOCATION(HttpStatus.OK, "LOC-005", "위치 삭제 성공"),
+    MODIFIED_LOCATION(HttpStatus.OK, "LOC-006", "위치 수정 성공"),
+    REGISTERED_PIN(HttpStatus.CREATED, "LOC-007", "위치 핀 등록 성공"),
+    DELETED_PIN(HttpStatus.OK, "LOC-008", "위치 핀 삭제 성공"),
+
+    LOGGED_IN(HttpStatus.OK, "USER-001", "로그인 성공"),
+    LOGGED_OUT(HttpStatus.OK, "USER-003", "로그아웃 성공"),
+    ;
+
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
+
+    SuccessResponse(HttpStatus status, String code, String message) {
+        this.status = status;
+        this.code = code;
+        this.message = message;
+    }
+}
