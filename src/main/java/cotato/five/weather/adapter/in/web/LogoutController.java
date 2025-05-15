2e112db2e613fa3f4efca8ddf8aae50d7b71b288
kych4n <hades1@kakao.com>
2025-05-15 16:17:26 +0900

    feat [#8] 로그아웃
    - 쿠키를 파기합니다.

---
+++ src/main/java/cotato/five/weather/adapter/in/web/LogoutController.java
@@ -0,0 +1,30 @@
+package cotato.five.weather.adapter.in.web;
+
+import cotato.five.weather.common.ApiResponse;
+import cotato.five.weather.common.BaseResponse;
+import cotato.five.weather.common.SuccessResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "auth")
+public class LogoutController {
+    @PostMapping("/logout")
+    public ResponseEntity<BaseResponse<?>> logout() {
+        return ApiResponse.success(SuccessResponse.LOGGED_OUT, expireCookie());
+    }
+
+    private ResponseCookie expireCookie() {
+        return ResponseCookie.from("id", "")
+                .httpOnly(true)
+                .secure(true)
+                .maxAge(0)
+                .path("/")
+                .build();
+    }
+}
