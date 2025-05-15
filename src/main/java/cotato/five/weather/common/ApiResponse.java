f081010b91c93b0d147e745851c2d3c843be24bb
kych4n <hades1@kakao.com>
2025-05-15 15:52:51 +0900

    feat [#8] 쿠키 설정을 위한 응답 추가

--- src/main/java/cotato/five/weather/common/ApiResponse.java
+++ src/main/java/cotato/five/weather/common/ApiResponse.java
@@ -1,10 +1,17 @@
 package cotato.five.weather.common;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 
 public class ApiResponse {
     public static <T> ResponseEntity<BaseResponse<?>> success(SuccessResponse successResponse) {
         return ResponseEntity.status(successResponse.getStatus()).body(BaseResponse.of(successResponse));
+    }
+
+    public static <T> ResponseEntity<BaseResponse<?>> success(SuccessResponse successResponse, ResponseCookie cookie) {
+        return ResponseEntity.status(successResponse.getStatus()).header(HttpHeaders.SET_COOKIE, cookie.toString())
+                .body(BaseResponse.of(successResponse));
     }
 
     public static <T> ResponseEntity<BaseResponse<?>> success(SuccessResponse successResponse, T data) {
