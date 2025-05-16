8fc55b43bbbc35828b77dcec9993e834b6b91728
sehyunnnn <kkshyun56@gmail.com>
2025-05-16 17:07:52 +0900

    refactor [#7] 구조 리팩토링

---
+++ src/main/java/cotato/five/weather/adapter/out/persistence/AirPollutionService.java
@@ -0,0 +1,43 @@
+package cotato.five.weather.adapter.out.persistence;
+
+import cotato.five.weather.application.dto.AirPollutionResponse;
+import cotato.five.weather.exception.BadRequestException;
+import cotato.five.weather.exception.UnauthorizedException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import static cotato.five.weather.common.FailureResponse.EXTERNAL_SERVER_ERROR;
+import static cotato.five.weather.common.FailureResponse.INTERNAL_SEVER_ERROR;
+
+@Service
+@RequiredArgsConstructor
+public class AirPollutionService {
+
+    private final RestTemplate restTemplate;
+    @Value("${openweather.api.key}")
+    private String API_KEY;
+
+    public AirPollutionResponse.Components getComponents(double lat, double lon) {
+        try {
+            String url = UriComponentsBuilder
+                    .fromHttpUrl("https://api.openweathermap.org/data/2.5/air_pollution")
+                    .queryParam("lat", lat)
+                    .queryParam("lon", lon)
+                    .queryParam("appid", API_KEY)
+                    .toUriString();
+
+            AirPollutionResponse response = restTemplate.getForObject(url, AirPollutionResponse.class);
+            return response.list().get(0).components();
+        } catch (HttpClientErrorException | ResourceAccessException e) {
+            throw new UnauthorizedException(EXTERNAL_SERVER_ERROR);
+        } catch (Exception e) {
+            throw new BadRequestException(INTERNAL_SEVER_ERROR);
+        }
+    }
+}
+
---
+++ src/main/java/cotato/five/weather/adapter/out/persistence/OpenWeatherService.java
@@ -0,0 +1,30 @@
+package cotato.five.weather.adapter.out.persistence;
+
+import cotato.five.weather.application.dto.*;
+import cotato.five.weather.application.port.in.WeatherClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OpenWeatherService implements WeatherClient {
+
+    private final CurrentWeatherService currentService;
+    private final HourlyWeatherService hourlyService;
+    private final WeeklyWeatherService weeklyService;
+
+    @Override
+    public WeatherDailyResponse getDailyWeather(double lat, double lon) {
+        return currentService.getDailyWeather(lat, lon);
+    }
+
+    @Override
+    public WeatherHourlyResponse getHourlyWeather(double lat, double lon) {
+        return hourlyService.getHourlyWeather(lat, lon);
+    }
+
+    @Override
+    public WeatherWeeklyResponse getWeeklyWeather(double lat, double lon) {
+        return weeklyService.getWeeklyWeather(lat, lon);
+    }
+}
