d2ef985fb3e46c58e64cf2ebdf4b73554dc78f82
kych4n <hades1@kakao.com>
2025-05-19 12:11:04 +0900

    feat [#19] 위치 목록 조회 시 위도, 경도, 핀 여부 추가
    - 위도, 경도는 날씨 조회 시 사용하고, 핀 여부는 핀이 없는 다른 위치와 구별하기 위해 사용하기 위해 추가했습니다.

--- src/main/java/cotato/five/weather/adapter/in/dto/response/LoadLocationResponse.java
+++ src/main/java/cotato/five/weather/adapter/in/dto/response/LoadLocationResponse.java
@@ -2,6 +2,9 @@ package cotato.five.weather.adapter.in.dto.response;
 
 public record LoadLocationResponse(
         Long id,
-        String name
+        String name,
+        Double latitude,
+        Double longitude,
+        Boolean isPinned
 ) {
 }
